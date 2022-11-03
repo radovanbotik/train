84825f3b2e7632cf5b12ddf4281e15d6d69aaab4
radovanbotik <radovanbotik@gmail.com>
2022-11-03 15:03:41 +0100

    ''

---
+++ 3.11.react/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [showEvents, setShowEvents] = useState(false);
+  const [forecast, setForecast] = useState([
+    { weather: "raining", id: 1 },
+    { weather: "sunny", id: 2 },
+    { weather: "windy", id: 3 },
+  ]);
+
+  const handleRemoval = id => {
+    setForecast(prev => {
+      return prev.filter(button => button.id !== id);
+    });
+  };
+
+  const buttons = forecast.map(weather => (
+    <button
+      key={weather.id}
+      id={weather.id}
+      onClick={() => handleRemoval(weather.id)}
+    >
+      {weather.weather}
+    </button>
+  ));
+
+  return (
+    <div className="App">
+      {showEvents && buttons}
+      {showEvents ? (
+        <button onClick={() => setShowEvents(false)}>Hide Events</button>
+      ) : (
+        <button onClick={() => setShowEvents(true)}>Display Events</button>
+      )}
+    </div>
+  );
+}
+
+export default App;
