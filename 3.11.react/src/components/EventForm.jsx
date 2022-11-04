4d31761443992aee41d14b0220f383e7f2082bc3
radovanbotik <radovanbotik@gmail.com>
2022-11-04 15:08:11 +0100

    ''

--- 3.11.react/src/components/EventForm.jsx
+++ 3.11.react/src/components/EventForm.jsx
@@ -1,14 +1,44 @@
 import React from "react";
+import { useState } from "react";
 import "./EventForm.css";
 
 export default function EventForm() {
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  //console.log({ title, date });
+
+  const resetForm = () => {
+    setTitle("");
+    setDate("");
+  };
+
   return (
     <form className="event-form">
       <label htmlFor="newevent">Weather</label>
-      <input type="text" id="newevent" name="newevent" />
+      <input
+        type="text"
+        id="newevent"
+        name="newevent"
+        value={title}
+        onChange={e => {
+          setTitle(e.target.value);
+        }}
+      />
       <label htmlFor="eventdate">Date</label>
-      <input type="date" id="eventdate" name="eventdate" />
+      <input
+        type="date"
+        id="eventdate"
+        name="eventdate"
+        value={date}
+        onChange={e => {
+          setDate(e.target.value);
+        }}
+      />
       <button type="submit">submit</button>
+      <p>
+        Event: {title} <br /> Date: {date}
+      </p>
+      <p onClick={resetForm}>reset the form</p>
     </form>
   );
 }
